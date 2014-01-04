3c532e3d705cdee581561d83e99f3b4ac560fa0b
Ely <sharontateresucitada@live.com.mx>
2014-01-04 01:57:38 -0600

    pagina uno

---
+++ pagina.php
@@ -0,0 +1,10 @@
+<link href="file:///C|/Users/Arely/Documents/Arely/paginascod/pruebap1/style.css" rel="stylesheet" type="text/css">
+<div id="principal">Colocar aquí el contenido para  id "principal"
+  <div id="Home">"home"</div>
+  <div class="adv">Colocar aquí el contenido para  class "adv1"</div>
+  <div class="adv">Colocar aquí el contenido para  class "adv2"</div>
+  <div class="adv">Colocar aquí el contenido para  class "adv3"</div>
+  <div class="adv">Colocar aquí el contenido para  class "adv4"</div>
+  <div id="chat">Colocar aquí el contenido para  id "chat"</div>
+  <div id="reproductor">Colocar aquí el contenido para  id "reproductor"</div>
+</div>
